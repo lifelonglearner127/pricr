d03208e42ab911f015060d688b779e3a2a852913
lifelonglearner127 <life.long.learner127@outlook.com>
2020-08-17 02:06:44 +0300

    PRC-46:GME-green mountain power spider created

--- src/spiders/__init__.py
+++ src/spiders/__init__.py
@@ -37,6 +37,7 @@ from .new_power_spider import NewPowerEnergySpider
 from .power_newx import PowerNextSpider
 from .v247_energy_spider import V247EnergySpider
 from .town_square_energy import TownSquareEnergySpider
+from .green_mountain_energy import GreenMountEnergySpider
 
 
 REP_SPIDER_MAPPING = {
@@ -77,5 +78,6 @@ REP_SPIDER_MAPPING = {
     "NEWP": NewPowerEnergySpider,
     "PWRN": PowerNextSpider,
     "V247": V247EnergySpider,
-    "TOWN": TownSquareEnergySpider
+    "TOWN": TownSquareEnergySpider,
+    "GME": GreenMountEnergySpider
 }

0cc8c07f0f1fef0159793344f7c372b8f3da74ee
lifelonglearner127 <life.long.learner127@outlook.com>
2020-08-16 07:20:56 -0700

    PRC-45: entrust energy spider created

---
+++ src/libs/expected_conditions/attribute_contains.py
@@ -0,0 +1,16 @@
+class element_has_attribute(object):
+    """An expectation for checking that an element has a particular css class.
+
+    locator - used to find the element
+    returns the WebElement once it has the particular css class
+    """
+    def __init__(self, locator):
+        self.locator = locator
+
+    def __call__(self, driver):
+        element = driver.find_element(
+            *self.locator)   # Finding the referenced element
+        if element.get_attribute("style") == 'display: none;':
+            return element
+        else:
+            return False

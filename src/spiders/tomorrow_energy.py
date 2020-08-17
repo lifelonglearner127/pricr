c63ef3ed9286dab121d304ecdedbbf9be0a6b0ec
lifelonglearner127 <life.long.learner127@outlook.com>
2020-08-17 00:39:12 -0700

    PRC-51: tomorrow energy spider created

---
+++ src/spiders/tomorrow_energy.py
@@ -0,0 +1,124 @@
+import re
+from typing import Tuple, Generator, Optional, List
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from ..libs.engines import SpiderBase
+from ..libs.models import TomorrowEnergyEntry
+
+
+class TomorrowEnergySpider(SpiderBase):
+    name = 'TOMORROW ENERGY'
+    REP_ID = 'TOM'
+    base_url = 'https://tomorrowenergy.com/'
+
+    def convert_to_entry(
+            self, zipcode: str, data: dict) -> TomorrowEnergyEntry:
+        return TomorrowEnergyEntry(
+            rep_id=self.REP_ID,
+            zipcode=zipcode,
+            **data
+        )
+
+    def extract(self, zipcode: str) -> List[TomorrowEnergyEntry]:
+        self.log("Searching with zip code - %s" % zipcode)
+        self.submit_zipcode(zipcode)
+        self.hook_after_zipcode_submit()
+        for elements in self.get_elements():
+            for element in elements:
+                entry = self.convert_to_entry(
+                    zipcode,
+                    self.analyze_element(element)
+                )
+                if entry.is_efl_link_exist:
+                    self.log("Downloading for <%s>..." % entry.product_name)
+                    if self.wait_until_download_finish():
+                        entry.filename = self.rename_downloaded(
+                            zipcode, entry.product_name
+                        )
+                self.data.append(entry)
+
+    def submit_zipcode(self, zipcode: str):
+        zipcode_element = self.wait_until(
+            'form#top-homepage-zip-widget input.input-field.zip',
+            By.CSS_SELECTOR
+        )
+        zipcode_element.clear()
+        zipcode_element.send_keys(zipcode)
+        zipcode_element.send_keys(Keys.ENTER)
+
+    def hook_after_zipcode_submit(self):
+        self.wait_until_invisible('large-loader')
+
+    def get_elements(self) -> Generator[Tuple[WebElement], None, None]:
+        self.wait_until_invisible('large-loader')
+        container = self.wait_until('energy-rates-container', By.CLASS_NAME)
+        elements = container.find_elements_by_css_selector(
+            'div.rate-plans-container div.rate-card')
+        retries = 0
+        while retries < 5 and not elements:
+            retries += 1
+            elements = container.find_elements_by_css_selector(
+                'div.rate-plans-container div.rate-card')
+        yield tuple(elements)
+
+    def analyze_element(self, el: WebElement):
+        try:
+            term_element = el.find_element_by_css_selector(
+                'div.rate-wrapper > div.tabs-wrapper ' +
+                'div.tab-block.active div.term-block div.term')
+        except Exception:
+            term_element = el.find_element_by_css_selector(
+                'div.rate-wrapper > div.tabs-wrapper ' +
+                'div.tab-block div.term-block div.term')
+
+        term = term_element.text
+        match = re.search(r'(\d+)\s+MONTH', term)
+        if match:
+            term = match.groups()[0]
+        else:
+            raise Exception("Term could not match. (%s)" % term)
+
+        try:
+            price_element = el.find_element_by_css_selector(
+                'div.rate-wrapper > div.tabs-wrapper ' +
+                'div.tab-block.active div.price-block span.amount')
+        except Exception:
+            price_element = el.find_element_by_css_selector(
+                'div.rate-wrapper > div.tabs-wrapper ' +
+                'div.tab-block div.price-block span.amount')
+
+        price = price_element.text
+
+        plan_element = el.find_element_by_css_selector(
+            'div.card-header div.name')
+        product_name = plan_element.text
+
+        is_efl_link_exist = False
+        efl_link_element = el.find_element_by_css_selector(
+            'div.rate-wrapper > div.tabs-wrapper ' +
+            'div.full-plan-details div.plan-documents ' +
+            'ul.links li.rate-plan-summary')
+        if efl_link_element.get_attribute('data-file-type') == "efacts":
+            is_efl_link_exist = True
+            self.client.get(
+                efl_link_element.get_attribute('data-summary-url'))
+
+        return {
+            'term': term,
+            'price': price,
+            'product_name': product_name,
+            'is_efl_link_exist': is_efl_link_exist
+        }
+
+    def wait_until_invisible(
+        self,
+        identifier: str,
+        by: str = By.ID,
+        timeout: int = 15
+    ) -> Optional[WebElement]:
+        return WebDriverWait(
+            self.client, timeout).until(
+                EC.invisibility_of_element_located((by, identifier)))

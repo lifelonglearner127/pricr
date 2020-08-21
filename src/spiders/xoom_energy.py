c00aa27a1112ac9bda5cc0423a8eddca7ae59430
lifelonglearner127 <life.long.learner127@outlook.com>
2020-08-21 16:11:38 +0300

    PRC-63:XOOM: xoom energy spider created

---
+++ src/spiders/xoom_energy.py
@@ -0,0 +1,67 @@
+import re
+from typing import Tuple, Generator
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.by import By
+from ..libs.engines import SpiderBase
+
+
+class XoomEnergySpider(SpiderBase):
+    name = "Xoom Energy"
+    REP_ID = "XOOM"
+    base_url = 'https://xoomenergy.com/'
+
+    def submit_zipcode(self, zipcode: str):
+        zipcode_element = self.wait_until(
+            '//input[@id="cpTopMain_cpMain_txtSearchHome"]',
+            by=By.XPATH
+        )
+        self.wait_for()
+        zipcode_element.clear()
+        zipcode_element.send_keys(zipcode)
+        rates_btn = self.client.find_element_by_xpath(
+            '//input[@id="cpTopMain_cpMain_btnSearch"]'
+        )
+        rates_btn.click()
+
+    def get_elements(self) -> Generator[Tuple[WebElement], None, None]:
+        self.wait_for(3)
+        container = self.wait_until(
+            '//section[@id="secRates"]',
+            by=By.XPATH
+        )
+        elements = container.find_elements_by_xpath(
+            './/div[@class="dashboard-content"]/div[1]/div/div/div/div[1]')
+        yield tuple(elements)
+
+    def analyze_element(self, el: WebElement):
+
+        # Popping up learn more details
+        detail_link = el.find_element_by_xpath(
+            './/a[@class="utility-details-trigger"]/button')
+        self.client.execute_script("arguments[0].click();", detail_link)
+
+        price_element = el.find_element_by_xpath(
+            './/div[@class="utility-choice-price"]'
+        ).text
+        price = re.search(r"(\d+(\.\d+)?)", price_element).group()
+
+        product_name = el.find_element_by_xpath(
+            './/div[@class="utility-choice-title"]').text
+
+        match = re.search(r"\b\d+\b", product_name)
+        if match:
+            term = match.group()
+        else:
+            term = 1
+        self.wait_for(2)
+        efl_element = self.client.find_element_by_xpath(
+            '//ul[@class="utility-documents"]/li[6]/a'
+        )
+        efl_url = efl_element.get_attribute('href')
+        self.client.get(efl_url)
+
+        return {
+            "term": term,
+            "price": price,
+            "product_name": product_name,
+        }
